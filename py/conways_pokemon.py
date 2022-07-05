f26c614b55551f399e7ad77ec35484eda567d26b
Federico Martino <fedemartino@gmail.com>
2022-07-05 17:37:55 -0300

    histogram working

--- py/conways_pokemon.py
+++ py/conways_pokemon.py
@@ -46,7 +46,7 @@ types_colors2 = [
     (183, 183, 197)
 ]
 
-efectiveness = [
+efectiveness_gen1 = [
     [1,1,1,1,1,1,1,1,1,1,1,1,0.5,0,1],
     [1,0.5,0.5,1,2,2,1,1,1,1,1,2,0.5,1,0.5],
     [1,2,0.5,1,0.5,1,1,1,2,1,1,1,2,1,0.5],
@@ -122,59 +122,48 @@ class Board:
         new_board = self.clone()
         for x in range(self.width):
             for y in range(self.height):
-                att = self.board[x][y]
-                count = 0
+                defn = self.board[x][y]
+                looseCount = 0
+                winners = []
                 for i in range(-1,2):
                     for j in range(-1,2):
                         if (i == 0 and j == 0) or (x+i < 0 or x+i >= self.width or y+j < 0 or y+j >= self.height):
                             continue
-                        defn = self.board[x+i][y+j]
+                        att = self.board[x+i][y+j]
                         if effectiveness[att][defn] == 2:
-                            new_board.board[x+i][y+j] = att
-                            self.histogram[att] += 1
-                            self.histogram[defn] -= 1
-                        #elif effectiveness[att][defn] == 1 and random.random() <= 0.5:
-                        #    new_board.board[x+i][y+j] = att
+                        # or (effectiveness[att][defn] == 1 and random.random() <= 0.0):
+                            looseCount = looseCount + 1
+                            winners.append(att)
+                if (looseCount > 0):
+                    winner = random.choice(winners)
+                    new_board.board[x][y] = winner
+                    self.histogram[winner] += 1
+                    self.histogram[defn] -= 1
         self.board = new_board.board
         
     def play(self, n):
         # Create subplots
         figure, ax = plt.subplots(1,2)
         mat = ax[0].matshow(self.board, cmap=c)
-        bar = ax[1].bar(len(types), self.histogram, 0.5)
+        bar = ax[1].bar(types, self.histogram)
+        [bar[i].set_color(normalized_colors[i]) for i in range(len(normalized_colors))]
         ax[1].set_xticks(np.arange(len(types)), types)
-        #ax[1].legend()
-        #print(self.histogram)
-        #rects2 = ax.bar(x + width/2, women_means, width, label='Women')
-
-        # Add some text for labels, title and custom x-axis tick labels, etc.
-        #ax.set_ylabel('Scores')
-        #ax.set_title('Scores by group and gender')
-        #ax.set_xticks(x, labels)
-        #ax.legend()
-        #bar.xticks(range(len(types)), types)
-
-        #objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
-        #y_pos = np.arange(len(objects))
-        #performance = [10,8,6,4,2,1]
-
-        #plt.bar(y_pos, performance, align='center', alpha=0.5)
-        #plt.xticks(y_pos, objects)
-        #plt.ylabel('Usage')
-        #plt.title('Programming language usage')
-
-        
+                
         # GUI
         plt.ion()
-        plt.axis('off')
+        #plt.axis('off')
 
         for i in range(n):
             self.get_next_state()
             mat.set_data(self.board)
-            #bar.(self.histogram)
+
+            bar.remove()
+            bar = ax[1].bar(types, self.histogram)
+            [bar[i].set_color(normalized_colors[i]) for i in range(len(normalized_colors))]
+            
             figure.canvas.draw()
             figure.canvas.flush_events()            
-            plt.pause(0.1)
+            plt.pause(0.05)
         plt.show()
 
     def drawBoard(self):
@@ -184,8 +173,9 @@ class Board:
 
 if __name__ == '__main__':
     
-    width = int(input("Width: "))
-    height = int(input("Height: "))
+    width = int(input("Width & Height (square board): "))
+    height = width
+    n = int(input("Number of iterations: "))
     board = Board(width, height)
-    board.play(300)
+    board.play(n)
 
